1065eb830793b1019f35196b32986c58e68f8cee
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 04:21:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -58,7 +58,60 @@ def game_hash
       ]
     },
     :away => {
-      
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }
+      ]
     }
   }
 end
