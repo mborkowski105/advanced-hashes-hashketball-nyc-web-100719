3251ad8e80d9659785c3bc5bc0c0b3d922a6024c
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 06:04:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,8 +206,7 @@ def player_stats(players_name)
       if (attributes == :players)
         data.each do |player|
           if player[:player_name] == players_name
-            new_hash = player.delete_if do |k, v|
-              k == :player_name
+            new_hash = player
             end
           end
         end
