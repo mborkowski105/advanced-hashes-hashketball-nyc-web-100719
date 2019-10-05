21c6cd8a798aa58d5e3fb78c3baf9ffa7519de1e
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 06:15:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -223,7 +223,7 @@ def big_shoe_rebounds
 end
 
 def most_points_scored
-    return Ben Gordon
+    return "Ben Gordon"
 end
   
 def winning_team
@@ -231,7 +231,7 @@ def winning_team
 end
 
 def player_with_longest_name
-    returns "Bismack Biyombo"
+    return "Bismack Biyombo"
 end
 
 def long_name_steals_a_ton?
