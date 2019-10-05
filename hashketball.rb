b9159138cee908b88db2bddec8776d9f55f4822e
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 04:18:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,24 @@
 # Write your code here!
-
+def game_hash
+  return {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+          
+        },
+        {},
+        {},
+        {},
+        {}
+        ]
+    },
+    :away => {
+      
+    }
+  }
+end
 
 
 
