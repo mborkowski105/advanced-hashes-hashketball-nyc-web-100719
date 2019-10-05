1a9867b5e08c09ee0f9847b52492140c324dd00f
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 05:17:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,21 +129,6 @@ end
 def num_points_scored(player)
   local_game_hash = game_hash()
   
-  if (player == "Alan Anderson" || "Reggie Evans" || "Brook Lopez" || "Mason Plumlee" || "Jason Terry")
-    local_game_hash[:home][:players].each do |p|
-      if (player == p[:name])
-        puts p[:points]
-        return p[:points]
-      end
-    end
-  else
-    local_game_hash[:away][:players].each do |p|
-      if (player == p[:name])
-        puts p[:points]
-        return p[:points]
-      end
-    end
-  end
 end
 
 
