368c734073f74304bb2fded653efe2821f7e67c7
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 04:34:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,39 +2,41 @@
 def game_hash
   return {
     :home => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
         {
-          :name => "",
+          :name => "Alan Anderson",
           :number => 0,
-          :shoe => 0,
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
         },
         {
-          :number => 0,
-          :shoe => 0,
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
         },
         {
-          :number => 0,
-          :shoe => 0,
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :name => "Brook Lopez"
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
         },
         {
           :number => 0,
